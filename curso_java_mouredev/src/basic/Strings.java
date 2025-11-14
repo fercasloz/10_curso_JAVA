c78b1339367d2e0564218148a08c95729367949f
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-11-14 13:57:43 +0100

    Create Strings.java

---
+++ curso_java_mouredev/src/basic/Strings.java
@@ -0,0 +1,22 @@
+package basic;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        //Cadenas de texto
+
+        String name = "Fer";
+
+        new String("Fern");
+        var surname = new String("Fernando");
+
+
+        // Operaciones básicas
+
+        System.out.println(name + " " + surname);
+
+        System.out.println(name.length());
+
+
+    }
+}
