bcb259983f25e92986967b153cbc7ac6c386d820
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-11-17 18:00:08 +0100

    Update ConditionalsExercises.java

--- curso_java_mouredev/src/basicExercises/ConditionalsExercises.java
+++ curso_java_mouredev/src/basicExercises/ConditionalsExercises.java
@@ -5,8 +5,24 @@ public class ConditionalsExercises {
     public static void main(String[] args) {
 
         // 1. Establece la edad de un usuario y muestra si puede votar (mayor o igual a 18).
+        int edad = 18;
+        if(edad >= 18){
+            System.out.println("El usuario puede votar porque tiene " + edad + " años");
+        }else{
+            System.out.println("El usuario no puede votar porque tiene " + edad + " años");
+        }
 
         // 2. Declara dos números y muestra cuál es mayor, o si son iguales.
+
+        int num1 = 10;
+        int num2 = 10;
+        if(num1 > num2){
+            System.out.println(num1 + " es mayor que " + num2);
+        }else if(num2 > num1){
+            System.out.println(num2 + " es mayor que " + num1);
+        }else{
+            System.out.println("Los números son iguales");
+        }
 
         // 3. Dado un número, verifica si es positivo, negativo o cero.
 
