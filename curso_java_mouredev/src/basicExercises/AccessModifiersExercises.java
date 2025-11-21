1751af2f9abea9d26f02e43b7a467696b310dc59
Fernando Castillo Lozano <fernando25798@gmail.com>
2025-11-21 09:53:19 +0100

    AccessModifiersExercises
    AccessModifiersExercises finished

--- curso_java_mouredev/src/basicExercises/AccessModifiersExercises.java
+++ curso_java_mouredev/src/basicExercises/AccessModifiersExercises.java
@@ -54,21 +54,68 @@ public class AccessModifiersExercises {
         System.out.println("\n");
 
         // 6. Crea una clase User con los atributos privados username y password. Implementa los métodos setUsername(String username), setPassword(String password) y checkPassword(String inputPassword) que compare contraseñas.
-
+        System.out.println("---------------Ejercicio 6---------------");
+        User user1 = new User("fercasloz","fercasloz1");
+        user1.checkPassword("fercasloz1");
+        System.out.println(user1.checkPassword("fercasloz1"));
+        user1.setPassword("Fercasloz1");
+        System.out.println(user1.checkPassword("fercasloz1"));
 
         System.out.println("\n");
 
 
         // 7. Crea una clase Employee con el atributo privado salary. Agrega el método raiseSalary(double percent) que solo permita aumentos positivos.
-
+        System.out.println("---------------Ejercicio 7---------------");
+        Employee employee1 = new Employee(2500);
+        employee1.showSalary();
+        employee1.raiseSalary(3000);
+        employee1.showSalary();
+        employee1.raiseSalary(2500);
+        employee1.showSalary();
         System.out.println("\n");
+
         // 8. Crea una clase Rectangle con los atributos privados width y height. Agrega setters y el método calculateArea() que devuelva el resultado de width * height.
-
+        System.out.println("---------------Ejercicio 8---------------");
+        Rectangle1 rectangle1 = new Rectangle1(5,10);
+        System.out.println("El area del rectangulo es : " +
+                                    rectangle1.calculateArea());
+        rectangle1.setWidth(10);
+        rectangle1.setHeight(15);
+        System.out.println("El area del rectangulo es : " +
+                                    rectangle1.calculateArea());
         System.out.println("\n");
+
+
         // 9. Crea una clase Student con el atributo privado grade. Agrega los métodos setGrade(int grade) y isPassed() que retorne true si la nota es mayor o igual a 60.
-
+        System.out.println("---------------Ejercicio 9---------------");
+        Student1 student1 = new Student1(65);
+        if(student1.isPassed()){
+            System.out.println("El estudiante ha aprobado");
+        }else{
+            System.out.println("El estudiante no ha aprobado");
+        }
+        Student1 student2 = new Student1(45);
+        if(student2.isPassed()){
+            System.out.println("El estudiante ha aprobado");
+        }else{
+            System.out.println("El estudiante no ha aprobado");
+        }
         System.out.println("\n");
+
         // 10. Crea una clase Car con el atributo privado speed. Agrega los métodos accelerate(int amount) que aumente la velocidad (máximo 120) y brake(int amount) que reduzca la velocidad (mínimo 0).
+        System.out.println("---------------Ejercicio 10---------------");
+        Car1 car1 = new Car1(90);
+        car1.showSpeed();
+        car1.accelerate(30);
+        car1.showSpeed();
+        car1.accelerate(20);
+        car1.showSpeed();
+
+        car1.brake(80);
+        car1.showSpeed();
+        car1.brake(80);
+        car1.showSpeed();
+
     }
 }
 
@@ -167,3 +214,118 @@ class Temperature{
         System.out.println("La temperatura es " + this.celsius + " Celsius");
     }
 }
+
+
+class User{
+    private String username, password;
+    public User(String username, String password){
+        this.username=username;
+        this.password=password;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+    public void setPassword(String password){
+        this.password = password;
+    }
+    public Boolean checkPassword(String password){
+        if(this.password.equals(password)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+class Employee{
+    private Double salario;
+    public Employee(double salario){
+        this.salario = salario;
+    }
+
+    public void raiseSalary(double salario){
+        if(salario >= this.salario && salario >= 0) {
+            this.salario = salario;
+        }else{
+            System.out.println("Solo se permiten aumentos positivos del salario");
+        }
+    }
+
+    public void showSalary(){
+        System.out.println("El salario del empleado es de " + this.salario);
+    }
+}
+
+class Rectangle1{
+    private double width,height;
+
+    public Rectangle1(double width, double height){
+        this.width=width;
+        this.height=height;
+    }
+
+    public void setWidth(double width){
+        this.width = width;
+    }
+    public void setHeight(double height){
+        this.height = height;
+    }
+
+    public double calculateArea(){
+        double area = this.height * this.width;
+        return area;
+    }
+}
+
+class Student1{
+    private int grade;
+    public Student1 (int grade){
+        this.grade = grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+
+    public boolean isPassed(){
+        if(this.grade >= 60){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+// 10. Crea una clase Car con el atributo privado speed. Agrega los métodos accelerate(int amount) que aumente la velocidad (máximo 120) y brake(int amount) que reduzca la velocidad (mínimo 0).
+
+class Car1{
+    private int speed;
+    public Car1(int speed){
+        if (speed >= 0 && speed <= 120){
+            this.speed = speed;
+        }
+    }
+
+    public void accelerate(int amount){
+        if(this.speed + amount <= 120){
+            this.speed = this.speed + amount;
+        }else{
+            System.out.println("No se puede acelerar por encima de 120km/h");
+        }
+    }
+
+    public void brake(int amount){
+        if(this.speed - amount >= 0){
+            this.speed = this.speed - amount;
+        }else{
+            System.out.println("No se puede ir a meneos de 0km/h");
+        }
+    }
+
+    public void showSpeed(){
+        System.out.println("El coche va a una velocidad de : " + this.speed + " Km/h");
+    }
+}
+
+
